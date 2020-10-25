02b7fa2b36b852dbfec1b82212d3adb7a9297eb6
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-10-25 12:59:16 +0100

    Introduces extensions extracting the result type.

--- src/IsAwaitable/Evaluation/EvaluationCache.cs
+++ src/IsAwaitable/Evaluation/EvaluationCache.cs
@@ -11,7 +11,7 @@ namespace IsAwaitable
 
         public static bool TryGet(
             Type type,
-            [NotNullWhen(true)] out TypeEvaluation evaluation)
+            [NotNullWhen(true)] out TypeEvaluation? evaluation)
         {
             if (_cache.TryGetValue(type, out evaluation))
                 return true;
--- src/IsAwaitable/IsAwaitableExtension.cs
+++ src/IsAwaitable/IsAwaitableExtension.cs
@@ -1,4 +1,5 @@
-﻿using IsAwaitable;
+﻿using System.Diagnostics.CodeAnalysis;
+using IsAwaitable;
 
 namespace System.Threading.Tasks
 {
@@ -88,13 +89,44 @@ namespace System.Threading.Tasks
         /// }
         /// </code>
         /// </example>
-        public static bool IsAwaitableWithResult(this object? instance)
+        public static bool IsAwaitableWithResult(this object? instance) =>
+            instance.IsAwaitableWithResult(out _);
+
+        /// <summary>
+        /// Determines whether the given instance can be awaited
+        /// and the operation will return a result.
+        /// </summary>
+        /// <param name="instance">The instance to be inspected.</param>
+        /// <param name="resultType">
+        /// The type of the result of the <c>await</c> operation.
+        /// Null when the <paramref name="instance" /> is not an awaitable
+        /// that returns a result.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> when the instance can be awaited and returns a result,
+        /// or <c>false</c> when it can't or is null.
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// object instance = argument;
+        /// if (instance.IsAwaitableWithResult(out var resultType))
+        /// {
+        ///     var result = await (dynamic)instance;
+        /// }
+        /// </code>
+        /// </example>
+        public static bool IsAwaitableWithResult(
+            this object? instance,
+            [NotNullWhen(true)] out Type? resultType)
         {
             if (instance is null)
+            {
+                resultType = null;
                 return false;
+            }
 
             var type = instance.GetType();
-            return type.IsAwaitableWithResult();
+            return type.IsAwaitableWithResult(out resultType);
         }
 
         /// <summary>
@@ -118,10 +150,49 @@ namespace System.Threading.Tasks
         /// }
         /// </code>
         /// </example>
-        public static bool IsAwaitableWithResult(this Type type)
+        public static bool IsAwaitableWithResult(this Type type) =>
+            type.IsAwaitableWithResult(out _);
+
+        /// <summary>
+        /// Determines whether instances of the given <c>Type</c> can be awaited
+        /// and the operation will return a result.
+        /// </summary>
+        /// <param name="type">The <c>Type</c> to be inspected.</param>
+        /// <param name="resultType">
+        /// The type of the result of the <c>await</c> operation.
+        /// Null when the <paramref name="type" /> is not an awaitable
+        /// that returns a result.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> when the type matches the language specification for
+        /// awaitable expressions returning a result, or <c>false</c> when it doesn't.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when the type parameter is null.
+        /// </exception>
+        /// <example>
+        /// <code>
+        /// T instance = argument;
+        /// if (typeof(T).IsAwaitableWithResult(out var resultType))
+        /// {
+        ///     var result = await (dynamic)instance;
+        /// }
+        /// </code>
+        /// </example>
+        public static bool IsAwaitableWithResult(
+            this Type type,
+            [NotNullWhen(true)] out Type? resultType)
         {
             var evaluation = GetEvaluationFor(type);
-            return evaluation.IsAwaitableWithResult;
+
+            if (evaluation.IsAwaitableWithResult)
+            {
+                resultType = evaluation.ResultType;
+                return true;
+            }
+
+            resultType = null;
+            return false;
         }
 
         private static TypeEvaluation GetEvaluationFor(Type type)
@@ -129,8 +200,8 @@ namespace System.Threading.Tasks
             if (type is null)
                 throw new ArgumentNullException(nameof(type));
 
-            if (type.IsKnownAwaitableWithResult())
-                return TypeEvaluation.AwaitableWithResult;
+            if (type.IsKnownAwaitableWithResult(out var resultType))
+                return TypeEvaluation.AwaitableWithResult(resultType);
 
             if (type.IsKnownAwaitable())
                 return TypeEvaluation.Awaitable;
