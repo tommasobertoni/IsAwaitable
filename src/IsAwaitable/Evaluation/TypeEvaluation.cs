d15f51c9b2499f601146d68e4cded3f753a11294
Tommaso Bertoni <tommasobertoni@users.noreply.github.com>
2020-10-25 12:17:50 +0100

    Enhances TypeEvaluation.

---
+++ src/IsAwaitable/Evaluation/TypeEvaluation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IsAwaitable
+{
+    internal class TypeEvaluation
+    {
+        public static readonly TypeEvaluation NotAwaitable = new TypeEvaluation(false);
+
+        public static readonly TypeEvaluation Awaitable = new TypeEvaluation(true);
+
+        public static TypeEvaluation AwaitableWithResult(Type resultType) =>
+            new TypeEvaluation(true, resultType);
+
+        public bool IsAwaitable { get; }
+
+        public bool IsAwaitableWithResult { get; }
+
+        /// <exception cref="System.InvalidOperationException">
+        /// Thrown when <see cref="IsAwaitableWithResult" /> returns <c>false</c>. />
+        /// </exception>
+        public Type ResultType
+        {
+            get
+            {
+                if (_resultType is null)
+                    throw new InvalidOperationException("Type doesn't return a type.");
+
+                return _resultType;
+            }
+        }
+
+        private readonly Type? _resultType;
+
+        private TypeEvaluation(
+            bool isAwaitable,
+            Type? resultType = null)
+        {
+            if (_resultType == typeof(void))
+                throw new ArgumentException("Result type can't be 'void'.");
+
+            IsAwaitable = isAwaitable;
+            _resultType = resultType;
+
+            IsAwaitableWithResult = isAwaitable && !(_resultType is null);
+        }
+    }
+}
